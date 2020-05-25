4336fe23aebe47515b2b1114aa189ef2675fc153
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-05-25 14:42:40 -0300

    define new method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,9 @@
 class GamesController < ApplicationController
   def new
+    array = ('A'..'Z').to_a
+    number = (4..10).to_a
+    number = number.sample.to_i
+    @array = array.sample(number).join
   end
 
   def score
